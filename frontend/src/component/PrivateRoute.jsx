f3f14cfd9345cb47bfb3755b8a99e5f8e07b49bb
aghil <aghilpuduruthy680623@gmail.com>
2025-04-03 12:41:12 +0530

    feventh commit

--- frontend/src/component/PrivateRoute.jsx
+++ frontend/src/component/PrivateRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate, Outlet } from "react-router-dom";
-import useAuth from "../hooks/useAuth.js";
+import useAuth from "../hooks/useauth.js";
 
 const PrivateRoute = ({ allowedRoles }) => {
   const { user, isLoading } = useAuth();
