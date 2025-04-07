31d2238b67bcffea5d53c74382c87e89b4184362
aghil <aghilpuduruthy680623@gmail.com>
2025-04-07 17:43:56 +0530

    change commit

--- frontend/src/axios/axiosInstance.js
+++ frontend/src/axios/axiosInstance.js
@@ -1,7 +1,18 @@
+// import axios from "axios";
+
+// const axiosInstance = axios.create({
+//   baseURL: "http://localhost:5000/",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   withCredentials: true,
+// });
+
+// export default axiosInstance;
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://fullstack-ecommeres.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
