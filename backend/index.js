4016a1ede355c40317cc3cb46051a24a5942e7dd
aghil <aghilpuduruthy680623@gmail.com>
2025-04-07 17:51:53 +0530

    change commit

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 app.use(cookieParser()); 
 app.use(cors({
-    origin: "http://localhost:5173", 
+    origin: "https://fullstack-ecommeres-1.onrender.com", 
     methods: "GET,POST,PUT,DELETE", 
     credentials: true
   }));
